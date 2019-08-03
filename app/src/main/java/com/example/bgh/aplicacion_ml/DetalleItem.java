0ee9a03ca38909567708c988ea18e18258b5031b
Constanza Morillo <constanzamorillo1@gmail.com>
2019-08-03 18:05:33 -0300

    Cambios en visualizacion de item
    se agregaron dependencias

--- app/src/main/java/com/example/bgh/aplicacion_ml/DetalleItem.java
+++ app/src/main/java/com/example/bgh/aplicacion_ml/DetalleItem.java
@@ -3,9 +3,13 @@ package com.example.bgh.aplicacion_ml;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.squareup.picasso.Picasso;
+
 
 public class DetalleItem extends AppCompatActivity {
 
@@ -13,9 +17,22 @@ public class DetalleItem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_item);
-        TextView texto = (TextView)findViewById(R.id.textView);
         Intent intent = getIntent();
         String item = intent.getStringExtra("item");
-        texto.setText(item);
+        JsonObject objeto = new Gson().fromJson(item, JsonObject.class);
+        TextView datos = findViewById(R.id.datos);
+        /*Se genera el string con datos del producto*/
+        String acepta_mercado = objeto.get("accepts_mercadopago").getAsBoolean() ? "SI" : "NO";
+        String cantidad_disponible = objeto.get("available_quantity").getAsInt() == 0 ? "NO HAY DISPONIBILIDAD" : "Cantidad Disponible: "+objeto.get("available_quantity").getAsInt();
+        String string_datos = objeto.get("title").getAsString()+"\n";
+        string_datos+= "Precio: "+objeto.get("currency_id").getAsString()+" "+objeto.get("price").getAsString()+"\n";
+        string_datos+= "Acepta Mercado Pago: "+acepta_mercado+"\n";
+        string_datos+= cantidad_disponible+"\n";
+        string_datos+= "Cantidad vendida: "+objeto.get("sold_quantity").getAsString();
+        datos.setText(string_datos);
+
+        String imageUrl = objeto.get("thumbnail").getAsString();
+        ImageView im = findViewById(R.id.imagen);
+        Picasso.get().load(imageUrl).placeholder(R.drawable.ic_launcher_background).into(im);
     }
 }
